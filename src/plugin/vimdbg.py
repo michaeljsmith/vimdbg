b06406d3a46d8581deccb5f8a9390d0ba865ea51
Michael Smith <msmith@msmith.id.au>
2010-05-02 12:59:50 +1000

    GDB subprocess progress.

--- src/plugin/vimdbg.py
+++ src/plugin/vimdbg.py
@@ -1,4 +1,5 @@
 import vim
+import subprocess
 
 class Error(Exception): pass
 
@@ -8,6 +9,15 @@ class CreateBufferError(Error):
 class BufferMissingError(Error):
 	def __init__(self, msg): self.msg = msg
 
+class DebuggerAlreadyRunningError(Error):
+	def __init__(self, msg): self.msg = msg
+
+class DebuggerMissingError(Error):
+	def __init__(self, msg): self.msg = msg
+
+class DebuggerSpawnError(Error):
+	def __init__(self, msg): self.msg = msg
+
 session_id = 100
 def get_session_id():
 	global session_id
@@ -15,16 +25,48 @@ def get_session_id():
 	session_id += 1
 	return new_session_id
 
-class GdbSession(object):
-	def __init__(self):
+class Session(object):
+	def __init__(self, driver):
 		self.session_id = get_session_id()
+		self.driver = driver
 		self.log_window = LogWindow(self.session_id)
+
+	def display_log_window(self):
 		self.log_window.create_buffer()
 		self.log_window.display()
 		self.log_window.log_message("This is the log window.")
 
+	def start_debugger(self):
+		self.driver.start()
+
+	def stop_debugger(self):
+		self.driver.stop()
+
+	def shutdown(self):
+		try: self.stop_debugger()
+		except DebuggerMissingError: pass
+
 class GdbDriver(object):
-	pass
+	def __init__(self):
+		self.process = None
+
+	def start(self):
+		if self.process:
+			raise DebuggerAlreadyRunningError(
+					"Cannot start debugger: GDB is already running.")
+		try:
+			self.process = subprocess.Popen("gdb", shell=True, bufsize=1,
+					stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+					stderr=subprocess.STDOUT)
+		except OSError as e:
+			raise DebuggerSpawnError("Failed to start GDB: " + e.strerror)
+
+	def stop(self):
+		if not self.process:
+			raise DebuggerMissingError("Cannot stop debugger: GDB not running.")
+		self.process.terminate()
+		self.process.communicate()
+		self.process = None
 
 class BreakpointCollection(object):
 	pass
@@ -48,7 +90,6 @@ class LogWindow(object):
 		buf_name = "/DbgLog" + str(self.session_id)
 		cmd = "bad " + buf_name
 		vim.command(cmd)
-		print [b.name for b in vim.buffers]
 		bufs = [b for b in vim.buffers if b.name == buf_name]
 		try:
 			self.buffer = bufs[0]
--- src/plugin/vimdbg.vim
+++ src/plugin/vimdbg.vim
@@ -1,12 +1,24 @@
 let s:script_location = expand("<sfile>:h")
 let s:script_module = expand("<sfile>:t:r")
 
-exe 'python sys.path.append(r"' . s:script_location . '")'
-exe 'python try: reload(' . s:script_module . ")\n" . 'except NameError: pass'
-exe 'python import ' . s:script_module
-let create_gdb_session_cmd = "try:\n"
-let create_gdb_session_cmd .= "\tgdb_session = vimdbg.GdbSession()\n"
-let create_gdb_session_cmd .= "except " . s:script_module . ".Error as e:\n"
-let create_gdb_session_cmd .= "\tprint \"Dbg Error: \" + e.msg"
-exe 'python ' . create_gdb_session_cmd
+function! <Sid>DbgCommand(cmd)
+	let python_cmd = "try:\n"
+	let python_cmd .= "\t" . a:cmd . "\n"
+	let python_cmd .= "except " . s:script_module . ".Error as e:\n"
+	let python_cmd .= "\tprint \"Dbg Error: \" + e.msg"
+	exe 'python ' . python_cmd
+endfunction
+
+function! <Sid>LoadPython()
+	exe "python try: gdb_session.shutdown()\n" . 'except NameError: pass'
+	exe 'python sys.path.append(r"' . s:script_location . '")'
+	exe 'python try: reload(' . s:script_module . ")\n" . 'except NameError: pass'
+	exe 'python import ' . s:script_module
+endfunction
+
+call <Sid>LoadPython()
+call <Sid>DbgCommand("gdb_driver = vimdbg.GdbDriver()")
+call <Sid>DbgCommand("gdb_session = vimdbg.Session(gdb_driver)")
+call <Sid>DbgCommand("gdb_session.display_log_window()")
+call <Sid>DbgCommand("gdb_session.start_debugger()")
 
