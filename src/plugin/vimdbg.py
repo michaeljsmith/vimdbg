8b647a7d8a1cd5a437b4aff5933670ca5522ff20
Michael Smith <msmith@msmith.id.au>
2010-05-03 15:34:14 +1000

    Breakpoint progress.

--- src/plugin/vimdbg.py
+++ src/plugin/vimdbg.py
@@ -2,13 +2,6 @@ import vim
 import subprocess
 import threading
 
-def log(msg):
-	f = file('log', 'a+')
-	try:
-		f.write(msg)
-	finally:
-		f.close()
-
 class Error(Exception): pass
 
 class CreateBufferError(Error):
@@ -41,6 +34,21 @@ class ThreadAlreadyRunningError(Error):
 class DebuggerStdoutClosed(Error):
 	def __init__(self, msg): self.msg = msg
 
+class DriverBreakpointsAlreadyConnectedError(Error):
+	def __init__(self, msg): self.msg = msg
+
+class DriverBreakpointsNotConnectedError(Error):
+	def __init__(self, msg): self.msg = msg
+
+class DriverAlreadyRunningError(Error):
+	def __init__(self, msg): self.msg = msg
+
+class BreakpointMissingError(Error):
+	def __init__(self, msg): self.msg = msg
+
+class BreakpointAlreadyExistsError(Error):
+	def __init__(self, msg): self.msg = msg
+
 class Delegate(object):
 	def __init__(self):
 		self.hndlrs = {}
@@ -48,7 +56,7 @@ class Delegate(object):
 	def add_handler(self, id, hndlr):
 		self.hndlrs[id] = hndlr
 
-	def remove_handler(self, id, hndlr):
+	def remove_handler(self, id):
 		del self.hndlrs[i]
 
 	def signal(self, *args):
@@ -92,25 +100,41 @@ def get_session_id():
 	return new_session_id
 
 class Session(object):
-	def __init__(self, driver):
+	def __init__(self, driver, bps):
 		self.session_id = get_session_id()
 		self.driver = driver
 		self.driver_proxy = DriverProxy(driver)
 		self.log_window = LogWindow(self.session_id)
 		self.thread = None
 		self.message_queue = ThreadMessageQueue()
+		self.bps = bps
+		self.driver_connected_to_bps = False
 
 		# Display all communications in the log window.
 		self.driver_proxy.on_communication.add_handler(
 				self.log_window, self.log_window.log_message)
+		self.driver.on_log.add_handler(
+				self.log_window, self.log_window.log_message)
+
+		# Log all breakpoint operations.
+		def log_breakpoint_add(id, file, line):
+			self.log_window.log_message(
+					"Breakpoint added(" + str(id) + "): " +
+					file + "(" + str(line) + ")")
+		def log_breakpoint_remove(id, file, line):
+			self.log_window.log_message(
+					"Breakpoint removed(" + str(id) + "): " +
+					file + "(" + str(line) + ")")
+		self.bps.on_add.add_handler(self.log_window, log_breakpoint_add)
+		self.bps.on_remove.add_handler(self.log_window, log_breakpoint_remove)
 
 	def display_log_window(self):
 		self.log_window.create_buffer()
 		self.log_window.display()
+		self.log_window.log_message("bps = " + str(id(self.bps)))
 
 	def start_debugger(self):
-		log("start_debugger\n")
-		self.log_window.log_message("Starting debugger.")
+		self.log_window.log_message("Starting debugger.\n")
 		if self.thread:
 			raise ThreadAlreadyRunningError(
 					'Cannot start listen thread - already running.')
@@ -127,31 +151,52 @@ class Session(object):
 
 	def stop_debugger(self):
 		self.log_window.log_message("Stopping debugger.")
-		log("stop_debugger\n")
+		if self.driver_connected_to_bps:
+			self.disconnect_driver_from_breakpoints()
 		if not self.thread:
 			raise ThreadNotRunningError(
 					'Cannot stop debugger - listen thread not running.')
-		log("calling driver.stop\n")
 		self.driver.stop()
-		log("calling thread.join\n")
 		self.thread.join()
-		log("calling driver.read_all_pending\n")
 		self.driver_proxy.read_all_pending(self.message_queue)
-		log("stop_debugger returning\n")
+
+	def run_debugger(self):
+		if not self.driver_connected_to_bps:
+			self.connect_driver_to_breakpoints()
+		self.log_window.log_message("Running debugger.\n")
+		self.driver.run()
 
 	def shutdown(self):
-		log("shutdown\n")
 		try:
-			log("calling stop_debugger\n")
 			self.stop_debugger()
 		except DebuggerMissingError: pass
 
 	def update(self):
-		log("update\n")
 		if not self.thread:
 			raise ThreadNotRunningError(
 					'Cannot update dbg thread - listen thread not running.')
 		self.driver_proxy.read_all_pending(self.message_queue)
+
+	def connect_driver_to_breakpoints(self):
+		if self.driver_connected_to_bps:
+			raise DriverBreakpointsAlreadyConnectedError(
+					"Cannot connect driver to breakpoints - already connected.")
+		self.log_window.log_message('Connecting breakpoings.' + str(self.bps.bps))
+		self.bps.on_add.add_handler(self.driver, self.driver.add_breakpoint)
+		self.bps.on_remove.add_handler(self.driver, self.driver.remove_breakpoint)
+		for id, (file, line) in self.bps.bps.iteritems():
+			self.driver.add_breakpoint(id, file, line)
+		self.driver_connected_to_bps = True
+
+	def disconnect_driver_from_breakpoints(self):
+		if not self.driver_connected_to_bps:
+			raise DriverBreakpointsNotConnectedError(
+					"Cannot disconnect driver from breakpoints - not connected.")
+		self.bps.on_add.remove_handler(self.driver)
+		self.bps.on_remove.remove_handler(self.driver)
+		for id, (drvr_id, file, line) in self.driver.bps.iteritems():
+			self.driver.remove_breakpoint(id)
+		self.driver_connected_to_bps = False
 
 class DriverProxy(object):
 	def __init__(self, driver):
@@ -161,7 +206,6 @@ class DriverProxy(object):
 	def read(self, queue):
 		try:
 			msg = queue.pop()
-			log("driver_proxy: msg recv: " + str(msg) + "\n")
 		except IndexError:
 			raise QueueEmptyError('Tried to read from empty thread queue.')
 		meth, args = msg
@@ -175,8 +219,6 @@ class DriverProxy(object):
 			raise QueueCorruptError('Invalid args in queue: "' + e.msg + '".')
 
 	def read_all_pending(self, queue):
-		log("driver_proxy.read_all_pending()\n")
-		log("queue = " + str(queue.items))
 		while not queue.empty():
 			self.read(queue)
 
@@ -189,6 +231,9 @@ class DriverProxy(object):
 class GdbDriver(object):
 	def __init__(self):
 		self.process = None
+		self.bps = {}
+		self.running = False
+		self.on_log = Delegate()
 
 	def start(self):
 		if self.process:
@@ -202,41 +247,67 @@ class GdbDriver(object):
 			raise DebuggerSpawnError("Failed to start GDB: " + e.strerror)
 
 	def stop(self):
-		log('driver.stop\n')
 		if not self.process:
 			raise DebuggerMissingError("Cannot stop debugger: GDB not running.")
 		self.process.stdin.close()
-		log('driver.stop - waiting\n')
 		self.process.wait()
-		log('driver.stop - returning\n')
+
+	def run(self):
+		if not self.process:
+			raise DebuggerMissingError("Cannot run debugger: GDB not running.")
+		if self.running:
+			raise DriverAlreadyRunningError("Cannot start debugging: already debugging.")
+		self.process.stdin.write('-exec-run\n')
+
+	def set_file(self, file):
+		self.on_log.signal("Setting debug file: " + file + "\n")
+		if not self.process:
+			raise DebuggerMissingError("Cannot set file: GDB not running.")
+		if self.running:
+			raise DriverAlreadyRunningError("Cannot set file: GDB already running.")
+		self.process.stdin.write('-file-exec-and-symbols ' + file + '\n')
 
 	def listen(self, queue):
-		log('Listen thread starting to listen.\n')
 		while True:
 			ln = self.process.stdout.readline()
-			log('Listen thread: received line: ' + ln)
 			if not ln:
 				queue.append(('handle_eof', []))
 				break
 			else:
 				queue.append(('handle_communication', [ln]))
-		log('Listen thread ending.\n')
 		self.process = None
 
+	def add_breakpoint(self, id, file, line):
+		self.on_log.signal("Adding breakpoint: " + file + ":" + str(line) + "\n")
+		cmd = '-break-insert ' + file + ':' + str(line) + '\n'
+		self.process.stdin.write(cmd)
+
+	def remove_breakpoint(self, id):
+		asdf = self.doesntexist
+
 class BreakpointCollection(object):
-	pass
+	def __init__(self):
+		self.bps = {}
+		self.on_add = Delegate()
+		self.on_remove = Delegate()
 
-class BreakpointWindow(object):
-	pass
+	def add(self, id, file, line):
+		if id in self.bps:
+			raise BreakpointAlreadyExistsError(
+					"Cannot add breakpoint " + id + " - id already in use.")
+		self.bps[id] = (file, line)
+		self.on_add.signal(id, file, line)
 
-class WatchList(object):
-	pass
-
-class WatchWindow(object):
-	pass
+	def remove(self, id):
+		try:
+			file, line = self.bps[i]
+			del self.bps[i]
+		except KeyError:
+			raise BreakpointMissingError(
+					"Cannot remove breakpoint " + id + " - no such breakpoint.")
+		self.on_remove.signal(id, file, line)
 
 class LogWindow(object):
-
 	def __init__(self, session_id):
 		self.buffer = None
 		self.session_id = session_id
@@ -265,4 +336,5 @@ class LogWindow(object):
 		if not self.buffer:
 			raise BufferMissingError("Cannot display log message - buffer not created.")
 		self.buffer.append(msg)
-
+		for w in (w for w in vim.windows if w.buffer == self.buffer):
+			w.cursor = len(self.buffer), 1
--- src/plugin/vimdbg.vim
+++ src/plugin/vimdbg.vim
@@ -18,10 +18,20 @@ endfunction
 
 function! GdbStart()
 	call <Sid>LoadPython()
+	call <Sid>DbgCommand("next_bp_id = 1")
+	call <Sid>DbgCommand("breakpoints = vimdbg.BreakpointCollection()")
 	call <Sid>DbgCommand("gdb_driver = vimdbg.GdbDriver()")
-	call <Sid>DbgCommand("gdb_session = vimdbg.Session(gdb_driver)")
+	call <Sid>DbgCommand("gdb_session = vimdbg.Session(gdb_driver, breakpoints)")
 	call <Sid>DbgCommand("gdb_session.display_log_window()")
 	call <Sid>DbgCommand("gdb_session.start_debugger()")
+endfunction
+
+function! GdbFile(file)
+	call <Sid>DbgCommand("gdb_driver.set_file('" . a:file . "')")
+endfunction
+
+function! GdbRun()
+	call <Sid>DbgCommand("gdb_session.run_debugger()")
 endfunction
 
 function! GdbStop()
@@ -30,4 +40,11 @@ endfunction
 
 function! GdbUpdate()
 	call <Sid>DbgCommand("gdb_session.update()")
+endfunction
+
+function! GdbBreakpoint(file, line)
+	python print id(breakpoints)
+	let cmd = "breakpoints.add(next_bp_id, '" . a:file . "', " . a:line . ")"
+	call <Sid>DbgCommand(cmd)
+	call <Sid>DbgCommand("next_bp_id += 1")
 endfunction
